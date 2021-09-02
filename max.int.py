a168cc3f7184c16f96462bcaaccb64e8595d34a3
Wiolala <wioleta.zelek@gmail.com>
2021-09-02 20:07:26 +0000

    Describing algorithm

---
+++ max.int.py
@@ -0,0 +1,13 @@
+'''
+algorithmic description
+
+1. Program asks about number
+2. Creates a list with numbers which user will be adding, 
+because no number was puted in yet, list is empty
+3. Makes a while loop which runs for numbers bigger then 0
+4. Adds a number which was puted by user to the list which earlier created
+5. Asks about number again , until number which user is puting will be bigger than 0,
+the while loop will be running
+6. Choosing the bigger number from the list which created
+7. Printing the biggest number
+'''
